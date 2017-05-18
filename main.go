675d05bc08e6d7de828e7e1aa3469c4c165e09ec
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-05-18 20:14:58 +0200

    main: initial storage

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/pressly/chi"
 )
@@ -28,4 +29,30 @@ func submit(w http.ResponseWriter, r *http.Request) {
 }
 
 func open(w http.ResponseWriter, r *http.Request) {
+}
+
+var db storage
+
+type storage struct {
+	sync.RWMutex
+	data map[string]string
+}
+
+func (s *storage) Add(url string) (name string) {
+	name = generateName()
+	s.Lock()
+	s.data[name] = url
+	s.Unlock()
+	return name
+}
+
+func (s *storage) Get(name string) (url string, ok bool) {
+	s.RLock()
+	url, ok = s.data[name]
+	s.RUnlock()
+	return url, ok
+}
+
+func generateName() string {
+	return ""
 }
